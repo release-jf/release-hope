90ba7db4f026a4c71113a4c70f68598d62f18e7a
topjfy <topjfk@163.com>
2023-04-09 08:32:16 +0000

    feat(demo): 初始化版本
    初始化版本
    
    BREAKING CHANGE: 初始化版本

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # release-hope
-使用vuepress-theme-hope主题的提交
+
+使用vuepress-theme-hope主题的提交机制
+
+提交步骤：
+- pnpm run commit 设置并提交git消息
+- pnpm run release 发布tags版本
+
---
+++ packages/theme/src/client/modules/blog/components/BlogHero.ts
@@ -0,0 +1,110 @@
+import {
+  usePageFrontmatter,
+  usePageHeadTitle,
+  withBase,
+} from "@vuepress/client";
+import { isString } from "@vuepress/shared";
+import { type VNode, computed, defineComponent, h, ref } from "vue";
+
+import DropTransition from "@theme-hope/components/transitions/DropTransition";
+
+import { SlideDownIcon } from "./icons/icons.js";
+import { type ThemeBlogHomePageFrontmatter } from "../../../../shared/index.js";
+import defaultHeroBgImagePath from "../assets/hero.jpg";
+
+import "../styles/blog-hero.scss";
+
+export default defineComponent({
+  name: "BlogHero",
+
+  setup() {
+    const title = usePageHeadTitle();
+    const frontmatter = usePageFrontmatter<ThemeBlogHomePageFrontmatter>();
+
+    const hero = ref<HTMLElement>();
+    const heroImage = computed(() => frontmatter.value.heroImage || null);
+
+    const isFullScreen = computed(
+      () => frontmatter.value.heroFullScreen || false
+    );
+
+    const bgImage = computed(() => {
+      const { bgImage } = frontmatter.value;
+
+      return isString(bgImage)
+        ? withBase(bgImage)
+        : bgImage === false
+        ? null
+        : defaultHeroBgImagePath;
+    });
+
+    return (): VNode | null =>
+      frontmatter.value.hero === false
+        ? null
+        : h(
+            "div",
+            {
+              ref: hero,
+              class: [
+                "blog-hero",
+                {
+                  fullscreen: isFullScreen.value,
+                  "no-bg": !bgImage.value,
+                },
+              ],
+            },
+            [
+              bgImage.value
+                ? h("div", {
+                    class: "mask",
+                    style: [
+                      {
+                        background: `url(${bgImage.value}) center/cover no-repeat`,
+                      },
+                      frontmatter.value.bgImageStyle,
+                    ],
+                  })
+                : null,
+              h(DropTransition, { appear: true, delay: 0.04 }, () =>
+                heroImage.value
+                  ? h("img", {
+                      class: "hero-image",
+                      style: frontmatter.value.heroImageStyle,
+                      src: withBase(heroImage.value),
+                      alt: frontmatter.value.heroAlt || "hero image",
+                    })
+                  : null
+              ),
+              h(DropTransition, { appear: true, delay: 0.08 }, () =>
+                frontmatter.value.heroText === false
+                  ? null
+                  : h("h1", frontmatter.value.heroText || title.value)
+              ),
+              h(DropTransition, { appear: true, delay: 0.12 }, () =>
+                frontmatter.value.tagline
+                  ? h("p", {
+                      class: "description",
+                      innerHTML: frontmatter.value.tagline,
+                    })
+                  : null
+              ),
+              isFullScreen.value
+                ? h(
+                    "button",
+                    {
+                      type: "button",
+                      class: "slide-down-button",
+                      onClick: () => {
+                        window.scrollTo({
+                          top: hero.value!.clientHeight,
+                          behavior: "smooth",
+                        });
+                      },
+                    },
+                    [h(SlideDownIcon), h(SlideDownIcon)]
+                  )
+                : null,
+            ]
+          );
+  },
+});
